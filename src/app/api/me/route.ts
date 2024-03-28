a864eaa49fdaeb39ba9e2fd33182dde8d5712a49
trastanechora <maestro.trastanechora@tokopedia.com>
2024-03-28 09:49:22 +0700

    fix(auth): change session cookie name

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { decode } from 'next-auth/jwt';
 
 export async function GET(req: NextRequest) {
-  const sessionToken = req.cookies.get('next-auth.session-token');
+  const sessionToken = req.cookies.get(process.env.NEXTAUTH_SESSION_TOKEN_NAME || '');
   const currentAccount = await decode({
     token: sessionToken?.value as unknown as string,
     secret: process.env.NEXTAUTH_SECRET || '',

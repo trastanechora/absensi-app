4f1e306d173137b8b974596c8f7359ee38bdc8b5
trastanechora <maestro.trastanechora@tokopedia.com>
2024-05-11 10:53:10 +0700

    feat(app): add redis

--- next.config.js
+++ next.config.js
@@ -5,4 +5,17 @@ const withPWA = require('next-pwa')({
 
 module.exports = withPWA({
   // next.js config
+  async headers() {
+    return [
+      {
+        source: "/_next/static/media/marker-icon",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=1, stale-while-revalidate=59",
+          },
+        ],
+      },
+    ];
+  },
 })
